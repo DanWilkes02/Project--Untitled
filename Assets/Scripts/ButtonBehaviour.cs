36003c104c1c1096dcdc48e51a70da02e23af8b2
DanWilkes02 <daniel_wilkes@outlook.com>
2016-10-22 14:47:08 +0100

    Quick save
    I between versions! Stuff is going well, this is just a save of my
    working

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class ButtonBehaviour : MonoBehaviour {
+
+	public GameObject movedObject;
+	public int moveAmount = 5;
+
+	private bool isDoneOnce = false;
+
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if(isDoneOnce == false){
+			movedObject.transform.position += new Vector3 (0, moveAmount, 0);
+
+			isDoneOnce = true;
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D coll){
+		if (isDoneOnce == true) {
+
+			movedObject.transform.position -= new Vector3 (0, moveAmount, 0);
+
+			isDoneOnce = false;
+		}
+	}
+}
+
+
+
--- Assets/Scripts/EntityFall.cs
+++ Assets/Scripts/EntityFall.cs
@@ -1,9 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class EntityFall : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D coll){
+
+		//Destroys falling object 
 		Destroy (coll);
+		Debug.Log ("Destroyed!");
+
+		//Check if the falling object is the player 
+		if (coll.name == "Player") {
+			//Loads current scene (Restarts Level)
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);  
+		}
 	}
+
 }
